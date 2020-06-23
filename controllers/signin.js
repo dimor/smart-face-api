75584ad0ba737253c9567a22d4183a577748d048
Dima Max <dimorm@gmail.com>
2020-06-23 15:33:18 +0300

    fix validation

--- controllers/signin.js
+++ controllers/signin.js
@@ -8,7 +8,7 @@ const handleSignIn = (req,res,db,bcrypt)=>{
     validateEmail(email)?null:res.status(400).json('email invalid');
   }
 
-  const validateEmail=(email)=> {
+  function validateEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
