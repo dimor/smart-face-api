fd1455e907ac3b794552bade4330dafab1e6e5d8
Dima Max <dimorm@gmail.com>
2020-06-23 14:03:12 +0300

    email validation for signin and register controllers

--- controllers/register.js
+++ controllers/register.js
@@ -4,8 +4,17 @@ const handleRegister = (req,res,db,bcrypt)=>{
 
   if(!email || !name || !password){
     return res.status(400).json('incorrect form submission');
+  }else{
+
+    validateEmail(email)?null:res.status(400).json('invalid email');
+    
   }
 
+
+  const validateEmail=(email)=> {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 
   bcrypt.hash(password, null, null, function(err, hash) {
 
--- controllers/signin.js
+++ controllers/signin.js
@@ -4,6 +4,13 @@ const handleSignIn = (req,res,db,bcrypt)=>{
 
   if(!email || !password){
     return res.status(400).json('incorrect form submission');
+  }else{
+    validateEmail(email)?null:res.status(400).json('email invalid');
+  }
+
+  const validateEmail=(email)=> {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
   }
 
 
