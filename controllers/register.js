9daad57bb6851bc71cc767d87de9d97561339433
Dima Max <dimorm@gmail.com>
2020-07-04 15:24:09 +0300

    registration controller , email validation fix

--- controllers/register.js
+++ controllers/register.js
@@ -5,45 +5,41 @@ const handleRegister = (req,res,db,bcrypt)=>{
   if(!email || !name || !password){
     return res.status(400).json('incorrect form submission');
   }else{
-
-    validateEmail(email)?null:res.status(400).json('invalid email');
-    
-  }
-
-
-  const validateEmail=(email)=> {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+     if(!re.test(String(email).toLowerCase())){
+      return res.status(400).json('invalid email')
+     }else{
+       
+       bcrypt.hash(password, null, null, function(err, hash) {
+     
+       db.transaction(trx=>{
+     
+         trx.insert({
+           hash:hash,
+           email:email,
+         })
+         .into('login')
+         .returning('email')
+         .then(loginEmail=>{
+             return db('users')
+               .returning('*')
+               .insert({
+                 email:loginEmail[0],
+                 name:name,
+                 joined:new Date()
+               })
+               .then(response=>{
+                 res.json(response[0]);
+               })
+               .catch(err=>res.status(400).json('Something went wrong'))
+           })
+         .then(trx.commit)
+         .catch(trx.rollback);
+         })
+       })
+     };
+     }
   }
-
-  bcrypt.hash(password, null, null, function(err, hash) {
-
-  db.transaction(trx=>{
-
-    trx.insert({
-      hash:hash,
-      email:email,
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail=>{
-        return db('users')
-          .returning('*')
-          .insert({
-            email:loginEmail[0],
-            name:name,
-            joined:new Date()
-          })
-          .then(response=>{
-            res.json(response[0]);
-          })
-          .catch(err=>res.status(400).json('Something went wrong'))
-      })
-    .then(trx.commit)
-    .catch(trx.rollback);
-    })
-  })
-};
 
 
 module.exports ={
