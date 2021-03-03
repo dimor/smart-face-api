663ce855e1f575527757b948bf5496f8cd046081
Dima Max <dimorm@gmail.com>
2021-03-03 10:17:15 +0200

    logs

--- controllers/register.js
+++ controllers/register.js
@@ -2,6 +2,10 @@ const handleRegister = (req,res,db,bcrypt)=>{
 
   const {email,name,password} = req.body;
 
+  
+  console.log(req.body);
+
+
   if(!email || !name || !password){
     return res.status(400).json('incorrect form submission');
   }else{
