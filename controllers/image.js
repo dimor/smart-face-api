2bf3b203d769c381a0a2b68ec6143f7be5ce2b75
Dima Max <dimorm@gmail.com>
2021-03-10 20:20:58 +0200

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -12,36 +12,35 @@ const handleApiCall = (req, res, db) => {
   let stats = null;
   let userRank = null;
 
+
   app.models
     .predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
     .then(response => {
-
+   
       clarifaiResponse = response;
 
       dataExist = clarifaiResponse.outputs[0];
 
-      if (Object.keys(dataExist.data).length === 0) {
+      if(Object.keys(dataExist.data).length === 0) {
         facesAnalyzed = 0;
       } else {
         facesAnalyzed = dataExist.data.regions.length;
       }
     })
-    .then(() => {  // get user_faces,user_used from specific id
-        db.from('users').where('login_id', '=', 10)
-        .increment({
-          user_used: 1,
-          user_faces: facesAnalyzed
-        })
-        .returning(['user_faces', 'user_used'])
-        .then(userStats => {
-          stats = userStats;
-          console.log(stats); 
-          res.json({ 'stats': stats, 'clarifai': clarifaiResponse, 'rank': rank });
-        })
-        .then(()=>{ // return as response  the stats and the clarifai response to user
-        })
-        .catch(err=>res.status(400).json(err))
+    .then(()=>{
+       return db.from('users').where('login_id', '=', 10)
+      .increment({
+        user_used:1,
+        user_faces:facesAnalyzed
+      })
+      .returning(['user_faces','user_used'])
+      .then(stats => {
+        res.json({'stats': stats , 'clarifai':clarifaiResponse});
+      })
+      .catch(err => res.status(400).json(err));
     })
+    .catch(err => res.status(400).json(err));
+}
 
 
 
@@ -57,10 +56,10 @@ const handleApiCall = (req, res, db) => {
     // .then(()=>{ // return as response  the stats and the clarifai response to user
     //   return res.json({ 'stats': stats, 'clarifai': clarifaiResponse, 'rank': rank });
     // })
-    .catch (err => {
-      console.error(err);
-      res.status(400).json(err)
-    });
+    // .catch (err => {
+    //   console.error(err);
+    //   res.status(400).json(err)
+    // });
 }
 
 
