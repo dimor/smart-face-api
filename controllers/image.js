2bdd4bdba50b5bfe3616133d7b836296f2e46660
Dima Max <dimorm@gmail.com>
2021-03-10 18:59:48 +0200

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -9,6 +9,8 @@ const handleApiCall = (req, res, db) => {
   let clarifaiResponse= null;
   let facesAnalyzed = null;
   let dataExist = null;
+  let stats = null;
+  let rank = null;
 
   app.models
     .predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
@@ -25,16 +27,24 @@ const handleApiCall = (req, res, db) => {
       }
     })
     .then(()=>{
-       return db.from('users').where('login_id', '=', 10)
+        db.from('users').where('login_id', '=', 10)
       .increment({
         user_used:1,
         user_faces:facesAnalyzed
       })
       .returning(['user_faces','user_used'])
       .then(stats => {
-        res.json({'stats': stats , 'clarifai':clarifaiResponse});
+          stats = stats;
+      })
+      .then(()=> {
+        db.select('rank') 
+        .from(db.select(db.raw('*,rank() over(order by user_faces desc) as rank from users')).as('temp'))
+        .where({login_id:'6'})
+        .then(rank=> rank = rank)
       })
       .catch(err => res.status(400).json(err));
+    }).then(()=>{
+      return res.json({'stats': stats , 'clarifai':clarifaiResponse, 'rank':rank});
     })
     .catch(err => res.status(400).json(err));
 }
