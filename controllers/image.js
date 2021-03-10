1cb3e0efbc6039bbbdcb47a35dbb1ba8dbc3cf1d
Dima Max <dimorm@gmail.com>
2021-03-10 19:51:23 +0200

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -6,48 +6,50 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res, db) => {
 
-  let clarifaiResponse= null;
+  let clarifaiResponse = null;
   let facesAnalyzed = null;
   let dataExist = null;
   let stats = null;
-
+  let userRank = null;
 
   app.models
     .predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
     .then(response => {
-   
+
       clarifaiResponse = response;
 
       dataExist = clarifaiResponse.outputs[0];
 
-      if(Object.keys(dataExist.data).length === 0) {
+      if (Object.keys(dataExist.data).length === 0) {
         facesAnalyzed = 0;
       } else {
         facesAnalyzed = dataExist.data.regions.length;
       }
     })
-    .then(()=>{
-       return db.from('users').where('login_id', '=', 10)
-      .increment({
-        user_used:1,
-        user_faces:facesAnalyzed
-      })
-      .returning(['user_faces','user_used'])
-      .then(stats => {
-          stats = stats;
-      })
-      .then(()=> {
-        return  db.select('rank') 
-        .from(db.select(db.raw('*,rank() over(order by user_faces desc) as rank from users')).as('temp'))
-        .where({login_id:'6'})
-        .returning()
-        .then(rank=> {
-          return res.json({'stats': stats , 'clarifai':clarifaiResponse, 'rank':rank});
+    .then(() => {  // get user_faces,user_used from specific id
+      return db.from('users').where('login_id', '=', 10)
+        .increment({
+          user_used: 1,
+          user_faces: facesAnalyzed
         })
-      })
-      .catch(err => res.status(400).json(err));
+        .returning(['user_faces', 'user_used'])
+        .then(stats => {
+          stats = stats;
+        })
     })
-    .catch(err => res.status(400).json(err));
+    .then(() => {  // get rank of spesific id
+      return db.select('rank')
+        .from(db.select(db.raw('*,rank() over(order by user_faces desc) as rank from users')).as('temp'))
+        .where({ login_id: '6' })
+        .returning()
+        .then(userRank => {
+          rank = userRank
+        })
+    })
+    .then(()=>{ // return as response  the stats and the clarifai response to user
+      return res.json({ 'stats': stats, 'clarifai': clarifaiResponse, 'rank': rank });
+    })
+    .catch (err => res.status(400).json(err));
 }
 
 
