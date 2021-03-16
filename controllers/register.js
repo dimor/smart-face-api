50b0761da7950907233def94bb91329e33453488
Dima Max <dimorm@gmail.com>
2021-03-16 11:02:25 +0200

    form validation on register, insert code reformat

--- controllers/register.js
+++ controllers/register.js
@@ -1,51 +1,56 @@
-const handleRegister = (req, res, db, bcrypt) => {
+const handleRegister = async (req, res, db, bcrypt) => {
 
   const { email, name, password } = req.body;
 
-
-  console.log(req.body);
-
-
-  if (!email || !name || !password) {
-    return res.status(400).json('Incorrect form submission');
-  } else {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(String(email).toLowerCase())) {
-      return res.status(400).json('Enter valid email')
-    } else {
+  const Joi = require('joi');
 
 
-      bcrypt.hash(password, 10, function (err, hash) {
+  //Validation Schema 
+  const schema = Joi.object({
 
-        db.transaction(trx => {
-          return trx.insert({
-            login_hash: hash,
-            login_email: email,
-          },['login_id']).into('login')
-            .then(([{login_id}]) => {
-              return trx.insert({
-                user_name: name,
-                user_joined: new Date(),
-                user_faces:0,
-                user_used:0,
-                ['login_id']:login_id
-              }).into('users')
-            })
-        })
-          .then(response => res.json(response))
-          .catch(error =>res.status(400).json(error))
-      })
-    };
+    password: Joi.string().required(),
+
+    email: Joi.string()
+      .email({ minDomainSegments: 2 }).required(),
+
+    name: Joi.string().required()
+  });
+
+
+  //Credantials Validation
+  try {
+    const validate = await schema.validateAsync(req.body);
+  } catch (error) {
+    return res.status(400).json(error.details[0].message)
+  }
+
+
+  //Hash Passwords 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Insert Values to db
+  try {
+    await db.transaction(async trx => {
+      const [{ login_id }] = await trx.insert({
+        login_hash: hashedPassword,
+        login_email: email,
+      }, ['login_id']).into('login')
+
+      await trx.insert({
+        user_name: name,
+        user_joined: new Date(),
+        user_faces: 0,
+        user_used: 0,
+        ['login_id']: login_id
+      }).into('users')
+
+      return res.status(200).json('You have successfully registered.');
+    });
+  } catch (error) {
+    res.status(400).json(error);
   }
 }
-
-
-
-
-
-
-
-
 
 
 module.exports = {
